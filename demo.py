7a9b2952544c3d0dd9f0ab981007981300b87fb3
Fei Chang <fc1271@nyu.edu>
2020-11-20 00:15:18 +0800

    Add new optional positional argument

--- demo.py
+++ demo.py
@@ -1,13 +1,14 @@
-# author: Tiffany Timbers
-# date: 2020-01-15
+# author: Fei Chang
+# date: 2020-11-19
 
 """This script prints out docopt args.
-Usage: docopt.py <arg1> --arg2=<arg2> [--arg3=<arg3>]
+Usage: docopt.py <arg1> --arg2=<arg2> [--arg3=<arg3>] [<arg_new>]
 
 Options:
 <arg>             Takes any value (this is a required positional argument)
 --arg2=<arg2>     Takes any value (this is a required option)
 [--arg3=<arg3>]   Takes any value (this is an optional option)
+[<arg_new>]       Takes any value (this is an optional positonal argument)
 """
 
 from docopt import docopt

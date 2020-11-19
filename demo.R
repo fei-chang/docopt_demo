491e6ce345315b1b0ee0b532f101886caa468f55
Fei Chang <fc1271@nyu.edu>
2020-11-19 23:51:07 +0800

    Edit demo.R

--- demo.R
+++ demo.R
@@ -1,16 +1,18 @@
-# author: Tiffany Timbers
-# date: 2020-01-15
+# author: Fei Chang
+# date: 2020-11-19
 
 "This script prints out docopt args.
-Usage: demo.R <arg1> --arg2=<arg2> [--arg3=<arg3>]
+Usage: demo.R <arg1> --arg2=<arg2> [--arg3=<arg3>] [<new_arg>]
 
 Options:
 <arg>             Takes any value (this is a required positional argument)
 --arg2=<arg2>     Takes any value (this is a required option)
 [--arg3=<arg3>]   Takes any value (this is an optional option)
+[<new_arg>]       Takes any value (this is an optional positional argument)
 " -> doc
 
 library(docopt)
 opt <- docopt(doc)
+print(opt$new_arg)
 print(opt)
 print(typeof(opt))

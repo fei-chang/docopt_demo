3bc45ffe5df1c958362239ddf72b2d2c5590092c
Fei Chang <fc1271@nyu.edu>
2020-11-20 00:17:12 +0800

    Recover original file

--- demo.py
+++ demo.py
@@ -1,14 +1,13 @@
-# author: Fei Chang
-# date: 2020-11-19
+# author: Tiffany Timbers
+# date: 2020-01-15
 
 """This script prints out docopt args.
-Usage: docopt.py <arg1> --arg2=<arg2> [--arg3=<arg3>] [<arg_new>]
+Usage: docopt.py <arg1> --arg2=<arg2> [--arg3=<arg3>]
 
 Options:
 <arg>             Takes any value (this is a required positional argument)
 --arg2=<arg2>     Takes any value (this is a required option)
 [--arg3=<arg3>]   Takes any value (this is an optional option)
-[<arg_new>]       Takes any value (this is an optional positonal argument)
 """
 
 from docopt import docopt
